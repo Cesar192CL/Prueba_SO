5ec1063aa20e7d8cd3cc90306c89a7c6a54fdf1f
USUARIO <USUARIO@DESKTOP-CCMTCC7>
2025-02-11 12:20:31 -0600

    Proyecto

---
+++ src/datos/Datos.java
@@ -0,0 +1,11 @@
+package datos;
+
+public class Datos {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Mi Nombre es Cesar");
+        System.out.println("Es la clase de taller de sistemas operativos");
+    }
+    
+}
